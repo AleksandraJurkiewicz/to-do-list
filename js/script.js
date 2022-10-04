d4642bf18ae17c7baccdfde3833e96e8a75fc4e5
Aleksandra Jurkiewicz <aleksandra.jurkiewicz2003@gmail.com>
2022-10-04 13:22:51 +0200

    toggle task done

--- js/script.js
+++ js/script.js
@@ -27,13 +27,19 @@
         render();
     }
 
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+        render();
+    }
+
     const render = () => {
         let htmlString = "";
 
         for (const task of tasks) {
             htmlString += `
             <li${task.done ? " style=\"text-decoration: line-through\"" : ""}>
-            <button class="js-remove">usuń</button>        
+            <button class="js-remove">usuń</button>
+            <button class="js-done">zrobione?</button>        
             ${task.content}
             </li>
             `;
@@ -46,6 +52,14 @@
         removeButtons.forEach((removeButton, index) => {
             removeButton.addEventListener("click", () => {
                 removeTask(index);
+            });
+        });
+
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+        toggleDoneButtons.forEach((toggleDoneButton, index) => {
+            toggleDoneButton.addEventListener("click", () => {
+                toggleTaskDone(index);
             });
         });
     };
